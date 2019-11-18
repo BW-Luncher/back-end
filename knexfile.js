8a5de26bfec62464c77bfc1ead05e39309914955
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-17 16:04:56 -0800

    knexfile set-up

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,22 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      //DB_DEV_DATABASE = apiluncher
+      //DB_DEV_USER = gavindreyer
+      database: process.env.DB_DEV_DATABASE,
+      user: process.env.DB_DEV_USER
+    },
+    migrations: {
+      diretcory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
@@ -13,7 +24,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -26,18 +37,18 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
