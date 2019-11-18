d8a0548156d94bad115501130b4e83ea272f4419
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-18 11:27:48 -0800

    fixed login

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -3,20 +3,15 @@ const db = require('../data/dbConfig');
 module.exports = {
 	find,
 	add,
-	findbyId,
-	
-	
-
-
+	findbyId
 };
 
-function find() {
-	return db('users');
+function find(username) {
+	return db('users').where(username);
 }
 
 function add(body) {
-	return db('users')
-	.insert(body, 'id');
+	return db('users').insert(body, 'id');
 }
 
 // function add(item){
@@ -29,7 +24,6 @@ function add(body) {
 // }
 
 function findbyId(id) {
-	return db('users')
-	.where({id})
-	first()
+	return db('users').where({ id });
+	first();
 }
